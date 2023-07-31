288ade26a3a235941776e0fd3141516aceea6aec
albertase <albertukaegbu@gmail.com>
2023-07-31 12:01:50 +0100

    Refactor

--- src/components/LibraryForm.jsx
+++ src/components/LibraryForm.jsx
@@ -1,26 +1,53 @@
+import { useState } from 'react';
+import { Country, City } from 'country-state-city';
+import FlagsSelect from 'react-flags-select';
+// import 'react-flags-select/css/react-flags-select.css';
+import Select from 'react-select';
 
 const LibraryForm = () => {
+  const [selectedCountry, setSelectedCountry] = useState('');
+  const [selectedCity, setSelectedCity] = useState('');
+  const [cities, setCities] = useState([]);
+
+  const handleCountryChange = (countryCode) => {
+    setSelectedCountry(countryCode);
+    setSelectedCity('');
+
+    // Get cities of the selected country
+    const countryCities = City.getCitiesOfCountry(countryCode);
+    setCities(countryCities);
+  };
+
+  const handleCityChange = (selectedOption) => {
+    setSelectedCity(selectedOption ? selectedOption.value : '');
+  };
+
+  const countries = Country.getAllCountries();
+  const cityOptions = cities.map((city) => ({ value: city.name, label: city.name }));
+
   return (
     <div>
-        <form action="">
-            <div>
-            <label htmlFor="">Country</label>
-            <select name="" id="">
-                <option value="">Ng</option>
-                <option value="">Ng</option>
-            </select>
-            </div>
-
-            <div>
-            <label htmlFor="">City</label>
-            <select name="" id="">
-                <option value="">Enugu</option>
-                <option value="">Lagos</option>
-            </select>
-            </div>
-        </form>
+      <div>
+        <label htmlFor="country">Country</label>
+        <FlagsSelect
+          className="country-select"
+          countries={countries.map((country) => country.isoCode)}
+          onSelect={(countryCode) => handleCountryChange(countryCode)}
+          selected={selectedCountry}
+          searchable={true}
+        />
+      </div>
+      <div>
+        <label htmlFor="city">City</label>
+        <Select
+          options={cityOptions}
+          value={cityOptions.find((option) => option.value === selectedCity)}
+          onChange={handleCityChange}
+          isSearchable={true}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LibraryForm
+export default LibraryForm;
