e7a15a37951159a9efadd3306982683022a8c2e8
albertase <albertukaegbu@gmail.com>
2023-07-31 11:49:26 +0100

    Basic form

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import LibraryForm from './components/LibraryForm'
+
+function App() {
+
+  return (
+    <>
+      <LibraryForm/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/LibraryForm.jsx
@@ -0,0 +1,26 @@
+
+const LibraryForm = () => {
+  return (
+    <div>
+        <form action="">
+            <div>
+            <label htmlFor="">Country</label>
+            <select name="" id="">
+                <option value="">Ng</option>
+                <option value="">Ng</option>
+            </select>
+            </div>
+
+            <div>
+            <label htmlFor="">City</label>
+            <select name="" id="">
+                <option value="">Enugu</option>
+                <option value="">Lagos</option>
+            </select>
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default LibraryForm
